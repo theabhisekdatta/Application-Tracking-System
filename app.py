fc347fe3712d677abd94563dcbdf7d4822feec68
Abhisek Datta <abhi.datta07@gmail.com>
2024-02-21 23:39:49 +0530

    done intregration, need to deploy

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import streamlit as st
-import google.generativeai as genai
+# import google.generativeai as genai
 import os
 import PyPDF2 as pdf
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from utilities.text_matching import calculate_similarity
 from utilities.missing_keywords import find_missing_keywords
 from utilities.extract_summary import generate_summary
 
-load_dotenv()  # load all our environment variables
+# load_dotenv()  # load all our environment variables
 
 
-genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+# genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
 # # Load pre-trained Word2Vec model
 # model_file_path = "pretrain_word2vec-google-news-300"
@@ -18,10 +18,10 @@ genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 #     word2vec_model = pickle.load(f)
 
 
-def get_gemini_repsonse(input):
-    model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content(input)
-    return response.text
+# def get_gemini_repsonse(input):
+#     model = genai.GenerativeModel('gemini-pro')
+#     response = model.generate_content(input)
+#     return response.text
 
 
 def input_pdf_text(uploaded_file):
@@ -35,20 +35,20 @@ def input_pdf_text(uploaded_file):
 # Prompt Template
 
 
-input_prompt = """
-Hey Act Like a skilled or very experience ATS(Application Tracking System)
-with a deep understanding of tech field,software engineering,data science ,data analyst
-and big data engineer. Your task is to evaluate the resume based on the given job description.
-You must consider the job market is very competitive and you should provide 
-best assistance for improving thr resumes. Assign the percentage Matching based 
-on Jd and
-the missing keywords with high accuracy
-resume:{text}
-description:{jd}
+# input_prompt = """
+# Hey Act Like a skilled or very experience ATS(Application Tracking System)
+# with a deep understanding of tech field,software engineering,data science ,data analyst
+# and big data engineer. Your task is to evaluate the resume based on the given job description.
+# You must consider the job market is very competitive and you should provide
+# best assistance for improving thr resumes. Assign the percentage Matching based
+# on Jd and
+# the missing keywords with high accuracy
+# resume:{text}
+# description:{jd}
 
-I want the response in one single string having the structure
-{{"MissingKeywords:[]","Job Description Summary":""}}
-"""
+# I want the response in one single string having the structure
+# {{"MissingKeywords:[]","Job Description Summary":""}}
+# """
 
 # streamlit app
 st.title(":blue[Application Tracking System]")
@@ -69,8 +69,8 @@ if submit:
         jd_matching = calculate_similarity(jd, text)
         missing_keywords = find_missing_keywords(text, jd)
         summary = generate_summary(jd)
-        response = get_gemini_repsonse(input_prompt)
+        # response = get_gemini_repsonse(input_prompt)
         st.write("Job Description Matching: ", f"{jd_matching:.2f}%")
         st.text_area("Summary", value=summary, height=200)
         # st.write("Missing Keywords from the Resume: ", missing_keywords)
-        st.write(response)
+        # st.write(response)
